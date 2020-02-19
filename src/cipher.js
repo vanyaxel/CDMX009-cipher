c7ca63f64397b031a6b08c0b2686bf577fbbc660
vanyaxel <vanya_xel@live.com.mx>
2020-02-19 14:54:11 -0600

    quinto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
 const cipher = {
-  // ...
-};
+  encode: function (word, offset) {
+      let resultEncode = "";
 
-export default cipher;
+      for (var i = 0; i < word.length; i++) {
+          let wordCipher = word.charCodeAt(i);
+
+          if (wordCipher >= 65 && wordCipher <= 90) {
+              let formula = (wordCipher - 65 + offset) % 26 + 65;
+              let newLetter = String.fromCharCode(formula);
+
+              resultEncode += newLetter;
+          }
+      }
+      return resultEncode;
+
+  },
+
+
+  decode: function (word) {
+      let resultDecode = "";
+      resultDecode = word;
+      console.log(resultDecode)
+
+      return resultDecode;
+  }
+}
+
+
+
+export default cipher
+
+/*
+decode: function (resultEncode, offset) {
+  let resultDecode = "";
+
+  for (var i = 0; i < resultEncode.length; i++) {
+      let wordCipher = word.charCodeAt(i);
+
+      if (wordCipher >= 65 && wordCipher <= 90) {
+          let formula = (wordCipher - 65 - offset) % 26 + 65;
+          let newLetter = String.fromCharCode(formula);
+
+          resultDecode += newLetter;
+
+      }
+  }
+  return resultDecode;
+}
+  }
+export default cipher */
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
+generatedCode.onclick = function(){
+    let word = document.getElementById("phrase").value;
+    let offset = parseInt(document.getElementById("number").value);
+    let releasedCode = cipher.encode (word, offset);
+    console.log(releasedCode);
+    document.getElementById("encodeText").innerHTML = (releasedCode);
+
+}
+
+generatedDecode.onclick = function(){
+     let word = document.getElementById("phrase").value;
+     let releasedDecode = word;
+    console.log(releasedDecode);
+    document.getElementById("decodeText").innerHTML = (releasedDecode);
+}
+
 console.log(cipher);
+
+
+
